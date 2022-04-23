48f74e547af2434542572146b3dd7e5e26dbed2d
Diego Lopes <diegoplopes5@gmail.com>
2022-04-23 17:21:28 -0300

    ft_strlcpy.c

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 21:01:09 by dieperei          #+#    #+#             */
+/*   Updated: 2022/04/18 19:38:08 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	if (size < 1)
+	{
+		return (len);
+	}
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (i);
+}
