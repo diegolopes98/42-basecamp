0807d915b0b401d68c4e171ce40b4641516fd4ce
Diego Lopes <diegoplopes5@gmail.com>
2022-04-23 17:45:22 -0300

    ft_print_program_name.c

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 05:54:40 by dieperei          #+#    #+#             */
+/*   Updated: 2022/04/21 06:02:33 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	ft_putstr(argv[0]);
+	ft_putstr("\n");
+	return (0);
+}
