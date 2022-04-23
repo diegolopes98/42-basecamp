b09bfd1d26e5e6e4f4a6486b4ca68f45ac44ee10
Diego Lopes <diegoplopes5@gmail.com>
2022-04-23 17:45:33 -0300

    ft_print_params.c

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 06:08:51 by dieperei          #+#    #+#             */
+/*   Updated: 2022/04/21 06:10:53 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i++;
+	}
+	return (0);
+}
