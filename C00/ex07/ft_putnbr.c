49137623f5a4239555b55a785cf7b7ac080a1404
Diego Lopes <diegoplopes5@gmail.com>
2022-04-23 17:13:08 -0300

    ft_putnbr.c

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 16:27:09 by dieperei          #+#    #+#             */
+/*   Updated: 2022/04/09 23:44:42 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_get_abs_nbr(int nb)
+{
+	int	abs_nb;
+
+	abs_nb = nb;
+	if (nb < 0)
+	{
+		abs_nb = nb * -1;
+	}
+	return (abs_nb);
+}
+
+void	ft_putnbr(int nb)
+{
+	int	nb_to_print;
+	int	abs_nb_to_print;
+
+	nb_to_print = nb % 10;
+	abs_nb_to_print = ft_get_abs_nbr(nb_to_print);
+	if (nb / 10 != 0)
+	{
+		ft_putnbr(nb / 10);
+	}
+	if (nb < 0 && nb / 10 == 0)
+	{
+		ft_putchar('-');
+	}
+	ft_putchar(abs_nb_to_print + 48);
+}
