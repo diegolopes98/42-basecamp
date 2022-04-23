4c57e976e4e85be35f8fecb6a14782ad04cd3578
Diego Lopes <diegoplopes5@gmail.com>
2022-04-23 17:43:29 -0300

    ft_atoi.c

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 20:53:21 by dieperei          #+#    #+#             */
+/*   Updated: 2022/04/20 21:12:17 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	number;
+
+	i = 0;
+	sign = 1;
+	number = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+		if (str[i++] == '-')
+			sign *= -1;
+	while (str[i] >= '0' && str[i] <= '9')
+		number = (number * 10) + (str[i++] - '0');
+	return (number * sign);
+}
