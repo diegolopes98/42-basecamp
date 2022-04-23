8ffd378321d6b8a50c1a74edd79fde29b821de8b
Diego Lopes <diegoplopes5@gmail.com>
2022-04-23 17:15:51 -0300

    ft_ultimate_div_mod.c

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieperei <dieperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 23:31:21 by dieperei          #+#    #+#             */
+/*   Updated: 2022/04/11 23:32:39 by dieperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_div_mod(int *a, int *b);
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
